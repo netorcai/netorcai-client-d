b9dfd4dd18d46c83824bb781c9e524134e9ac1a8
Millian Poquet <millian.poquet@inria.fr>
2018-10-28 14:10:11 +0100

    [misc] put coverage results in /tmp

--- Makefile
+++ Makefile
@@ -1,3 +1,5 @@
+coverage_directory=/tmp/cover-netorcai-d
+
 default: library
 
 library:
@@ -10,9 +12,11 @@ unittest:
 	dub test
 
 unittest-cov:
+	mkdir -p $(coverage_directory)
 	dub test -b unittest-cov
 
 clean:
 	rm -f -- *.lst
 	rm -f -- libnetorcai-client.*
 	rm -f -- netorcai-client-*
+	rm -rf -- $(coverage_directory)
--- src/netorcai/package.d
+++ src/netorcai/package.d
@@ -2,3 +2,17 @@ module netorcai;
 
 public import netorcai.client;
 public import netorcai.message;
+
+// Write coverage results to /tmp instead of current directory
+version(D_Coverage) shared static this()
+{
+    import core.runtime : dmd_coverDestPath;
+    import std.file : exists, mkdir;
+    import std.format : format;
+
+    // The coverage directory must be created manually before executing the tests.
+    enum COVPATH = "/tmp/cover-netorcai-d";
+    dmd_coverDestPath(COVPATH);
+
+    assert(COVPATH.exists, format!"Coverage output directory '%s' does not exist. Please create it manually."(COVPATH));
+}
